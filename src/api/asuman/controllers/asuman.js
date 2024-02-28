8b1294c395eee2f12859ae0cc562a050f36d1e8e
unknown <asumanwebdev@gmail.com>
2024-02-28 13:38:49 +0300

    first commit

---
+++ src/api/asuman/controllers/asuman.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * asuman controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::asuman.asuman');
---
+++ src/api/asuman/services/asuman.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * asuman service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::asuman.asuman');
